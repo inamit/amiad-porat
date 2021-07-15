e88414ce19f9d1526a3da60e10654b2a776a654a
Amit <u9060254@bsmch.net>
2021-07-15 13:09:34 +0300

    feat(constants.dart): Add theme color variable

--- lib/constants.dart
+++ lib/constants.dart
@@ -16,6 +16,19 @@ const white = Color(0xFFF5F5F5);
 const grey = Color(0xFFC1C1C1);
 const neonBlue = Color(0xFF6D78FF);
 
+MaterialColor neonBlueMaterial = MaterialColor(0xFF6D78FF, {
+  50: Color(0xFF6D78FF).withOpacity(.1),
+  100: Color(0xFF6D78FF).withOpacity(.2),
+  200: Color(0xFF6D78FF).withOpacity(.3),
+  300: Color(0xFF6D78FF).withOpacity(.4),
+  400: Color(0xFF6D78FF).withOpacity(.5),
+  500: Color(0xFF6D78FF).withOpacity(.6),
+  600: Color(0xFF6D78FF).withOpacity(.7),
+  700: Color(0xFF6D78FF).withOpacity(.8),
+  800: Color(0xFF6D78FF).withOpacity(.9),
+  900: Color(0xFF6D78FF).withOpacity(1)
+});
+
 const redOrangeGradient = [red, orange];
 const whiteGreyGradient = [white, grey];
 
