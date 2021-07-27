b79500a3509d9fbeb4da3f569fe46402298b39bc
Amit <u9060254@bsmch.net>
2021-07-27 15:23:33 +0300

    feat(weekly_schedule.dart): refactor code #9

--- lib/screens/weekly_schedule/weekly_schedule.dart
+++ lib/screens/weekly_schedule/weekly_schedule.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:amiadporat/constants.dart';
 import 'package:amiadporat/data/lessons.dart';
 import 'package:amiadporat/models/lesson_block.dart';
@@ -32,65 +30,78 @@ class _WeeklyScheduleState extends State<WeeklySchedule> {
   Widget build(BuildContext context) {
     DateTime now = DateTime.now();
     List<Color> backgrounds = [red, orange, neonBlue];
+    var calendarHeader = HeaderStyle(
+      formatButtonVisible: false,
+      titleCentered: true,
+      leftChevronVisible: false,
+      rightChevronVisible: false,
+      headerPadding: EdgeInsets.all(18.0),
+    );
+
     return Column(
       children: [
         TableCalendar(
           locale: "he_IL",
           weekendDays: [DateTime.friday, DateTime.saturday],
           startingDayOfWeek: StartingDayOfWeek.sunday,
-          headerStyle: HeaderStyle(
-            formatButtonVisible: false,
-            titleCentered: true,
-            leftChevronVisible: false,
-            rightChevronVisible: false,
-            headerPadding: EdgeInsets.all(18.0),
-          ),
+          headerStyle: calendarHeader,
           focusedDay: now,
           firstDay: now.subtract(Duration(days: now.weekday)),
-          lastDay: DateTime.now().add(Duration(
-              days: DateTime.now().weekday < DateTime.thursday
-                  ? DateTime.thursday - DateTime.now().weekday
-                  : DateTime.thursday)),
+          lastDay: _getEndOfWeek(),
           calendarFormat: CalendarFormat.week,
           selectedDayPredicate: (day) {
             return isSameDay(_selectedDay, day);
           },
-          onDaySelected: (selectedDay, focusedDay) {
-            setState(() {
-              _selectedDay = selectedDay;
-              _selectedDayLessons = lessonsData
-                  .where((element) =>
-                      element.selectedDay!.isSameDate(_selectedDay))
-                  .toList();
-              _selectedDayLessons.sort((first, second) =>
-                  first.selectedDay!.difference(second.selectedDay!).inMinutes);
-            });
-          },
+          onDaySelected: _onDaySelected,
         ),
-        _selectedDayLessons.isEmpty
-            ? Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Text(
-                  "אין לך תגבורים ביום הזה",
-                  style: TextStyle(fontSize: 20.0),
-                ),
-              )
-            : Expanded(
-                child: ListView.builder(
-                    itemCount: _selectedDayLessons.length,
-                    itemBuilder: (context, index) {
-                      return HomepageCard(
-                          color: backgrounds[index % backgrounds.length],
-                          content: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: _lessonDetails(
-                              _selectedDayLessons[index].selectedSubject,
-                              _selectedDayLessons[index].selectedDay!,
-                            ),
-                          ));
-                    }),
-              ),
+        _selectedDayLessons.isEmpty ? _noLessons() : _lessonsList(backgrounds),
       ],
+    );
+  }
+
+  void _onDaySelected(selectedDay, focusedDay) {
+    setState(() {
+      _selectedDay = selectedDay;
+      _selectedDayLessons = lessonsData
+          .where((element) => element.selectedDay!.isSameDate(_selectedDay))
+          .toList();
+      _selectedDayLessons.sort((first, second) =>
+          first.selectedDay!.difference(second.selectedDay!).inMinutes);
+    });
+  }
+
+  DateTime _getEndOfWeek() {
+    return DateTime.now().add(Duration(
+        days: DateTime.now().weekday < DateTime.thursday
+            ? DateTime.thursday - DateTime.now().weekday
+            : DateTime.thursday));
+  }
+
+  Expanded _lessonsList(List<Color> backgrounds) {
+    return Expanded(
+      child: ListView.builder(
+          itemCount: _selectedDayLessons.length,
+          itemBuilder: (context, index) {
+            return HomepageCard(
+                color: backgrounds[index % backgrounds.length],
+                content: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: _lessonDetails(
+                    _selectedDayLessons[index].selectedSubject,
+                    _selectedDayLessons[index].selectedDay!,
+                  ),
+                ));
+          }),
+    );
+  }
+
+  Padding _noLessons() {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Text(
+        "אין לך תגבורים ביום הזה",
+        style: TextStyle(fontSize: 20.0),
+      ),
     );
   }
 
