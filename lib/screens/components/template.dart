42c76b9476e6554958ec7da00dba9f37a340a5b7
Amit <u9060254@bsmch.net>
2021-08-08 19:05:30 +0300

    feat(template.dart): Get authenticated user #13

--- lib/screens/components/template.dart
+++ lib/screens/components/template.dart
@@ -1,5 +1,8 @@
 import 'dart:async';
 
+import '../../models/student.dart';
+import '../../utils/auth.dart';
+import '../../utils/db.dart';
 import 'package:flutter/material.dart';
 
 import '../home/home.dart';
@@ -17,11 +20,26 @@ class _TemplateState extends State<Template> {
   int _selectedIndex = 0;
   List<Widget> pages = [Home(), WeeklySchedule()];
 
+  Student? student;
+
+  @override
+  void initState() {
+    getStudent();
+    super.initState();
+  }
+
+  Future<void> getStudent() async {
+    Student fetched = await DB().getStudentInfo(AuthHandler().currentUser!.uid);
+    setState(() {
+      student = fetched;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('שלום, עמית'),
+        title: Text('שלום, ${getName()}'),
       ),
       bottomNavigationBar: MyBottomNavBar(
         selectedIndex: _selectedIndex,
@@ -32,6 +50,14 @@ class _TemplateState extends State<Template> {
         child: pages[_selectedIndex],
       ),
     );
+  }
+
+  String getName() {
+    if (this.student != null) {
+      return this.student!.firstName;
+    }
+
+    return "";
   }
 
   FutureOr onGoBack(dynamic value) {
