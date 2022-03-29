40c5fdc3ec32cb1a620cff019714c3445c9e828f
Amit Inbar <inamit323@gmail.com>
2022-03-29 15:59:32 +0300

    Get lessons for student by date

--- lib/dal/lesson.dal.dart
+++ lib/dal/lesson.dal.dart
@@ -1,3 +1,6 @@
+import 'package:amiadporat/models/subjects.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 import '../models/lesson/tutorLesson/lesson.dart';
 import '../models/lesson/tutorLesson/studentStatus.dart';
 
@@ -25,5 +28,24 @@ class LessonDal {
     lessons.sort(((a, b) => a.data.date.compareTo(b.data.date)));
 
     return lessons.length > 0 ? lessons[0].data : null;
+  }
+
+  static Future<List<Lesson>> getLessonByUserAndDate(
+      String uid, DateTime date) async {
+    Map<String, String> student = new Map();
+    student.putIfAbsent('student', () => uid);
+    student.putIfAbsent('status',
+        () => StudentStatusHelper().getValue(StudentStatus.SCHEDULED));
+
+    QuerySnapshot<Lesson> lessons = await FirebaseFirestore.instance
+        .collection('lessons')
+        .where('date', isEqualTo: date)
+        .where('students', arrayContains: student)
+        .withConverter(
+            fromFirestore: LessonCollectionReference.fromFirestore,
+            toFirestore: LessonCollectionReference.toFirestore)
+        .get();
+
+    return lessons.docs.map((e) => e.data()).toList();
   }
 }
