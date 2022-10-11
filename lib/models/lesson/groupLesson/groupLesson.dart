eaa6abe9a740526004081de9d48bcae7a6cee7ca
Amit Inbar <inamit323@gmail.com>
2022-10-11 21:49:17 +0300

    feat: abstract

--- lib/models/lesson/groupLesson/groupLesson.dart
+++ lib/models/lesson/groupLesson/groupLesson.dart
@@ -3,9 +3,14 @@ import '../absLesson.dart';
 class GroupLesson extends AbsLesson {
   String? teacher;
 
+  static get groupLessonType => 'שיעור';
+
   GroupLesson(
       {required String subject, String? this.teacher, required DateTime date})
-      : super(lessonType: 'שיעור', subject: subject, date: date);
+      : super(
+            lessonType: GroupLesson.groupLessonType,
+            subject: subject,
+            date: date);
 
   @override
   String get title =>
